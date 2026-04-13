aba407b8eba53377775024811d9329687805f629
Jeerapas Kwansiri <jeerapas.kwansiri@gmail.com>
2026-04-13 11:10:45 +0700

    refactor: small revision on map command

--- command_map.go
+++ command_map.go
@@ -6,17 +6,17 @@ import (
 )
 
 func commandMap(cfg *config, args ...string) error {
-	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
+	locationAreas, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
 	if err != nil {
 		return err
 	}
 	fmt.Println("Location areas:")
-	for _, area := range resp.Results {
+	for _, area := range locationAreas.Results {
 		fmt.Printf(" - %s\n", area.Name)
 	}
 	fmt.Println()
-	cfg.nextLocationAreaURL = resp.Next
-	cfg.previousLocationAreaURL = resp.Previous
+	cfg.nextLocationAreaURL = locationAreas.Next
+	cfg.previousLocationAreaURL = locationAreas.Previous
 	return nil
 
 }
@@ -26,17 +26,17 @@ func commandMapb(cfg *config, args ...string) error {
 		return errors.New("You're on the first page\n")
 	}
 
-	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.previousLocationAreaURL)
+	locationAreas, err := cfg.pokeapiClient.ListLocationAreas(cfg.previousLocationAreaURL)
 	if err != nil {
 		return err
 	}
 	fmt.Println("Location areas:")
-	for _, area := range resp.Results {
+	for _, area := range locationAreas.Results {
 		fmt.Printf(" - %s\n", area.Name)
 	}
 	fmt.Println()
-	cfg.nextLocationAreaURL = resp.Next
-	cfg.previousLocationAreaURL = resp.Previous
+	cfg.nextLocationAreaURL = locationAreas.Next
+	cfg.previousLocationAreaURL = locationAreas.Previous
 	return nil
 
 }
